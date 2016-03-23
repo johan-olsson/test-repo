94e4dbc9ac1f45da81f8cc81c860421445c06473
Johan Olsson <johano.js@gmail.com>
2016-03-23 13:11:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World2!');
 });
 
 app.listen(3000, function () {
